cc62c7ed66392a9d359214a88df85a4b7213457b
dariomatias-dev <matiasdario75@gmail.com>
2026-01-16 06:41:28 -0300

    feat(components/buttons/link-button.tsx): added disable property
    - Implemented an option to disable the button.

--- components/buttons/link-button.tsx
+++ components/buttons/link-button.tsx
@@ -7,20 +7,24 @@ interface LinkButtonProps extends LinkProps {
   target?: HTMLAttributeAnchorTarget | undefined;
   className?: string;
   children: ReactNode;
+  disabled?: boolean;
 }
 
 export const LinkButton = ({
   target,
   className = "",
   children,
+  disabled = false,
   ...props
 }: LinkButtonProps) => {
   return (
     <Link
       {...props}
-      target={target}
+      target={disabled ? undefined : target}
       className={cn(
-        "group flex items-center gap-2 rounded-full bg-white px-6 py-2 text-[11px] font-black text-black transition-all hover:bg-zinc-50 active:scale-95 duration-300",
+        "group flex items-center gap-2 rounded-full bg-white px-6 py-2 text-[11px] font-black text-black transition-all duration-300",
+        "hover:bg-zinc-50 active:scale-95",
+        disabled && "opacity-50 pointer-events-none cursor-not-allowed",
         className
       )}
     >
